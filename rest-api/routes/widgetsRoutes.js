1fbedd3b8dd1d7814e19fceec311ee2c497a7d98
markiine <dmkotkas@gmail.com>
2022-12-22 14:23:50 +0200

    route update

--- rest-api/routes/widgetsRoutes.js
+++ rest-api/routes/widgetsRoutes.js
@@ -2,16 +2,22 @@ const widgets = require("../controllers/widgetsController")
 
 module.exports = (app) => {
 
-    //READ
-    app.get('/widgets', widgets.getAll)
-    app.get('/widgets/:id', widgets.getById)
+    app.route('/widgets')
+            .get(widgets.getAll)
+            .post(widgets.create)
 
+    app.route('/widgets/:id')
+            .get(widgets.getById)
+            .put(widgets.update)
+            .delete(widgets.delete)
+    
+    //READ        
+    //app.get('/widgets', widgets.getAll)
+    //app.get('/widgets/:id', widgets.getById)
     //CREATE
-    app.post('/widgets', widgets.create)
-
+    //app.post('/widgets', widgets.create)
     //UPDATE
-    app.put('/widgets/:id',widgets.update)
-
+    //app.put('/widgets/:id',widgets.update)
     //DELETE
-    app.delete('/widgets/:id',widgets.delete)
+    //app.delete('/widgets/:id',widgets.delete)
 }
