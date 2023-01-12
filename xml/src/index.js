5ec07149da696f130c0fd731b0b03eb0922ad63e
markiine <dmkotkas@gmail.com>
2023-01-12 14:55:22 +0200

    XML begin

---
+++ xml/src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `<table id ="xmlTable"></table>`
+
+const getXml=function(fileName){
+    const xmlhttp = new XMLHttpRequest()
+    xmlhttp.open("GET", fileName, false)
+    xmlhttp.send()
+    return xmlhttp.responseXML
+
+}
+
+const generateTable = function(XMLContent) {
+    let tableRows = "<tr><th>Title</th><th>Price</th></tr>"
+    const gameElements = XMLContent.getElementsByTagName("game")
+    for (let i = 0; i < gameElements.length; index++) {
+        const game = gameElements[i];
+        tableRows += "<tr><td>" +
+            game.getElementsByTagName("title")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            game.getElementsByTagName("title")[0].childNodes[0].nodeValue +
+            "</td></tr>"
+        
+    }
+    document.getElementById("xmlTable").innerHTML = tableRows
+}
